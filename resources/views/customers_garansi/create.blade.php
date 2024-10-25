f2f68db2569557fad15be5f6ac3ec77c4596efa4
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-25 17:11:18 +0700

    Update create.blade.php

--- resources/views/customers_garansi/create.blade.php
+++ resources/views/customers_garansi/create.blade.php
@@ -112,7 +112,7 @@
                 if (count > 1) {
 
                     html += ' <div class="input-group input-group-sm mb-3" id="dtTgl">'+
-                        '<input type="file" multiple="" class="form-control-file" name="garansi_photo[]" />';
+                        '<input type="file" multiple="" class="form-control-file" name="garansi_photo[]" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">';
                 }
                 var remove_button = '';
 
