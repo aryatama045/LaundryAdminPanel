09e9eeeb06393dd2e6742b74f36aabe332c0d3cd
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-25 17:23:34 +0700

    Update create.blade.php

--- resources/views/customers_garansi/create.blade.php
+++ resources/views/customers_garansi/create.blade.php
@@ -119,7 +119,7 @@
                 var remove_button = '';
 
                 if (count > 1) {
-                    remove_button = '<div class="input-group-append"><span style="height:100% !important" name="remove" class="input-group-text btn-danger default remove" id="inputGroup-sizing-sm"><i class="fa fa-plus"></i> Remove</span></div>';
+                    remove_button = '<div class="input-group-append"><button style="height:100% !important" name="remove" class="input-group-text btn-danger default remove" id="inputGroup-sizing-sm"><i class="fa fa-trash"></i> Remove</button></div>';
                 }
 
                 html += remove_button +
