6c8b12c3f3dcb51a0ce493090a11af298f9026d9
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-25 16:52:32 +0700

    Update create.blade.php

--- resources/views/customers_garansi/create.blade.php
+++ resources/views/customers_garansi/create.blade.php
@@ -66,17 +66,6 @@
                             </div>
 
                             <div class="row">
-                                <div class="col-12 col-md-6 mb-2 py-2">
-                                    <div class="div">
-                                        <label>{{ __('Bukti Foto') }}</label>
-                                        <input type="file" multiple="" class="form-control-file" name="garansi_photo[]">
-                                        <button class="float-right btn btn-primary "><i class="fa fa-plus-square"></i> Add</button>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div class="row">
                                 <div class="col-12 col-md-8 mb-2">
                                     <label>{{ __('Bukti Foto') }}</label>
 
@@ -84,6 +73,7 @@
                                         <div class="form-group mb-3" id="dtTgl">
                                             <div class="input-group typeahead-container" >
                                                 <div class="input-group">
+                                                    <span class="input-group-text input-group-append input-group-addon typeahead"><i class="fa fa-file-image-o"></i></span>
                                                     <input type="file" multiple="" id='dtfirst' class="form-control-file" name="garansi_photo[]">
                                                     <div class="input-group-append">
                                                         <button type="button" name="add" class="btn btn-primary default add">
