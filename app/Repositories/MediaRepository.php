9bafd606f3dd5f3743b22dd7bb4d512a66c454f5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-27 13:36:36 +0700

    Update MediaRepository.php

--- app/Repositories/MediaRepository.php
+++ app/Repositories/MediaRepository.php
@@ -57,7 +57,7 @@ class MediaRepository extends Repository
 
 
 
-    public function storeByGaransi( $file, string $path, string $description = null, string $type = null, $urutan): Media
+    public function storeByGaransi(UploadedFile $file, string $path, string $description = null, string $type = null, $urutan): Media
     {
 
         $date           = now()->toDateTimeString();
@@ -73,11 +73,8 @@ class MediaRepository extends Repository
 
         $foto_bukti = 'SMP_'.$kode.'_'.$jam.'X'.$menit.'-'.$urutan.'.'.$extension;
 
-        $path = Storage::put('/'. trim($path, '/'), $foto_bukti, 'public');
+        $path = Storage::put('/images/garansi/', $file, 'public');
 
-        $file->move($path, $foto_bukti);
-
-        $extension = $file->extension();
         if(!$type){
             $type = in_array($extension, ['jpg', 'png', 'jpeg', 'gif']) ? 'image' : $extension;
         }
