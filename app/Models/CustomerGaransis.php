6e1b6bd99e34030065a1af8c78506c64320741a5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-28 14:05:21 +0700

    Update CustomerGaransis.php

--- app/Models/CustomerGaransis.php
+++ app/Models/CustomerGaransis.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class CustomerGaransis extends Model
 {
     use HasFactory;
