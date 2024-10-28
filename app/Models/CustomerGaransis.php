ad460222e54ac80ecf21f14555621e45bcbdcdc9
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-28 14:07:52 +0700

    Update CustomerGaransis.php

--- app/Models/CustomerGaransis.php
+++ app/Models/CustomerGaransis.php
@@ -27,29 +27,5 @@ class CustomerGaransis extends Model
     }
 
 
-    // ----------------- Attribute
-    public function getProfilePhotoPathAttribute()
-    {
-        return $this->user->profilePhotoPath;
-    }
 
-    public function getNameAttribute()
-    {
-        return $this->user->first_name . ' ' . $this->user->last_name;
-    }
-
-    public function GaransiPhoto(): BelongsTo
-    {
-        return $this->belongsTo(Media::class, 'foto_id');
-    }
-
-
-    public function getBuktiFotoPathGaransi(): string
-    {
-        if ($this->GaransiPhoto && Storage::exists($this->GaransiPhoto->src)) {
-            return Storage::url($this->GaransiPhoto->src);
-        }
-
-        return asset('images/dummy/dummy-user.png');
-    }
 }
