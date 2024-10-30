dfee4f19316f4b9f1bc278afc943d04a3acdce69
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-30 17:11:04 +0700

    Update show.blade.php

--- resources/views/customers_klaim/show.blade.php
+++ resources/views/customers_klaim/show.blade.php
@@ -263,7 +263,7 @@
                                                                     </svg>
                                                                 </div>
                                                                 <div class="tracking-date">
-                                                                    <svg width="5%" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+                                                                    <svg style="width:10px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
                                                                         <circle class="path circle" fill="none" stroke="#db3646" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"></circle>
                                                                         <line class="path line" fill="none" stroke="#db3646" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="34.4" y1="37.9" x2="95.8" y2="92.3"></line>
                                                                         <line class="path line" fill="none" stroke="#db3646" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="95.8" y1="38" x2="34.4" y2="92.2"></line>
