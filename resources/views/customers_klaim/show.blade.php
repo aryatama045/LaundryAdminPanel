83ccfbdfc3ac59026efa150fccb9409264758fa5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-10-30 16:50:13 +0700

    Update show.blade.php

--- resources/views/customers_klaim/show.blade.php
+++ resources/views/customers_klaim/show.blade.php
@@ -239,7 +239,17 @@
                                                                     </svg>
                                                                 </div>
                                                                 <div class="tracking-date"><img src="https://raw.githubusercontent.com/shajo/portfolio/a02c5579c3ebe185bb1fc085909c582bf5fad802/delivery.svg" class="img-responsive" alt="order-placed" /></div>
-                                                                <div class="tracking-content">Order Placed<span>09 Aug 2025, 10:00am</span></div>
+                                                                <div class="tracking-content">Status Diterima<span>09 Aug 2025, 10:00am</span></div>
+                                                            </div>
+
+                                                            <div class="tracking-item">
+                                                                <div class="tracking-icon status-intransit">
+                                                                    <svg class="svg-inline--fa fa-circle fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                                                                    <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path>
+                                                                    </svg>
+                                                                </div>
+                                                                <div class="tracking-date"><img src="https://raw.githubusercontent.com/shajo/portfolio/a02c5579c3ebe185bb1fc085909c582bf5fad802/delivery.svg" class="img-responsive" alt="order-placed" /></div>
+                                                                <div class="tracking-content">Sedang Verifikasi<span>09 Aug 2025, 10:00am</span></div>
                                                             </div>
 
                                                             <div class="tracking-item-pending">
@@ -249,7 +259,7 @@
                                                                     </svg>
                                                                 </div>
                                                                 <div class="tracking-date"><img src="https://raw.githubusercontent.com/shajo/portfolio/a02c5579c3ebe185bb1fc085909c582bf5fad802/delivery.svg" class="img-responsive" alt="order-placed" /></div>
-                                                                <div class="tracking-content">Delivered<span>12 Aug 2025, 09:00pm</span></div>
+                                                                <div class="tracking-content">Verified<span>12 Aug 2025, 09:00pm</span></div>
                                                             </div>
                                                         </div>
 
