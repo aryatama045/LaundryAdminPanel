47fc42f6ada109f0453bc656d2de6e72b0b3f5be
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-11-01 16:10:25 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -31,8 +31,7 @@
                                 </div>
                                 <div class="card-icon">
                                     <div class="icon icon-shape text-white shadow">
-                                        <img hidden width="80" height="80" src="{{ asset('images/icons/items.svg') }}" alt="">
-                                        <img width="80" src="{{ asset('images/icons/services.svg') }}" alt="">
+                                        <img width="80" src="{{ asset('images/icons/items.svg') }}" alt="">
                                     </div>
                                 </div>
                             </div>
