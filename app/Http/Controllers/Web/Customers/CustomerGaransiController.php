c345eda6c0f76f332fd869aac8d47686bffaa50c
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-11-02 20:58:07 +0700

    Update CustomerGaransiController.php

--- app/Http/Controllers/Web/Customers/CustomerGaransiController.php
+++ app/Http/Controllers/Web/Customers/CustomerGaransiController.php
@@ -86,14 +86,22 @@ class CustomerGaransiController extends Controller
                 $date           = now()->toDateTimeString();
                 $jam            =  date('h',strtotime($date));
                 $menit          =  date('i',strtotime($date));
-                $text_wtr = 'Pukul  '.$jam.':'.$menit.' <br> Tanggal '.$tanggal;
+                $text_wtr1 = 'Pukul  '.$jam.':'.$menit;
+                $text_wtr2 = 'Tanggal '.$tanggal;
 
-                $img->text($text_wtr, 200, 150, function($font) {  
+
+
+                $img->text($text_wtr1, 200, 150, function($font) {  
                     $font->file(public_path('rabbit.ttf'));   //LOAD FONT-NYA JIKA ADA, SILAHKAN DOWNLOAD SENDIRI
                     $font->size(20);
-                    $font->color('#000');
-                    $font->align('bottom');
-                    $font->valign('right');
+                    $font->color('#fff');
+                    $font->align('right');
+                });
+                $img->text($text_wtr2, 200, 150, function($font) {  
+                    $font->file(public_path('rabbit.ttf'));   //LOAD FONT-NYA JIKA ADA, SILAHKAN DOWNLOAD SENDIRI
+                    $font->size(20);
+                    $font->color('#fff');
+                    $font->align('right');
                 });
 
                 $img->save(storage_path('app/public/' . $thumbnail->path)); //DAN SIMPAN JUGA KE DALAM FOLDER YG SAMA
