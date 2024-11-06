7e6dfacc9ce04d62bae1c5ea368b62c9b00d4403
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-11-06 16:26:51 +0700

    Update CustomerKlaimController.php

--- app/Http/Controllers/Web/Customers/CustomerKlaimController.php
+++ app/Http/Controllers/Web/Customers/CustomerKlaimController.php
@@ -131,7 +131,7 @@ class CustomerKlaimController extends Controller
 
 
                 $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
-                if($server != '"Windows"'){
+                if($server == '"Android"'){
                     $img = Image::read(storage_path('app/public/' . $thumbnail->path));
 
                     $logo = public_path('logo.png');
