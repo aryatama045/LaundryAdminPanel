2bb1ce2fa2715abecce9e29a68128214324dc975
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-11-16 14:07:05 +0700

    asd

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -102,7 +102,6 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
                     </div>
                 </div>
 
-                
                 @endif
 
 
@@ -135,6 +134,20 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
                 </div>
                 @endrole
 
+            </div>
+
+            <div class="row">
+                <section class="customer-logos slider">
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
+                    <div class="slide"><img src="http://www.webcoderskull.com/img/logo.png"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
+                    <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
+                </section>
             </div>
         </div>
 
