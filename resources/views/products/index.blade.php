e6e9439d610106fc46167595ca60ff59609d866a
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-11-21 11:15:04 +0700

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -25,7 +25,7 @@
                                     <li class="nav-item ml-2 mr-md-0">
                                         <a class="btn btn-success" data-bs-effect="effect-super-scaled"
                                             data-bs-toggle="modal" href="#modalImport">
-                                            <i class="fa fa-upload"></i> {{ localize('Import') }}
+                                            <i class="fa fa-upload"></i> Import
                                         </a>
                                     </li>
                                     @can('product.create')
