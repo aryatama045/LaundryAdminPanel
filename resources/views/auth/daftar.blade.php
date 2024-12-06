7f60ac25908f21dd5774d828308af70c7d13fc8e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-06 15:27:48 +0700

    Update daftar.blade.php

--- resources/views/auth/daftar.blade.php
+++ resources/views/auth/daftar.blade.php
@@ -29,7 +29,7 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
 
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-7 col-lg-6 login-form-section">
+            <div class="col-12 col-md-7 col-lg-8 login-form-section">
                 <div class="login">
                     <form role="form" class="pui-form" id="loginform" method="POST" action="{{ route('daftar') }}">
                         @csrf
@@ -44,7 +44,6 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
                                 {{ $message }}
                             @enderror
 
-
                             <h3>Daftar Customer</h3>
                             <p>Silahkan lengkapi form berikut:</p>
 
@@ -56,90 +55,80 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
                             </div>
                         @endif
 
-                        <div class="row">
-                                <div class="col-12 col-md-12 mb-2">
-                                    <label for="">{{ __('Company') }} </label>
-                                    <input type="text" class="form-control" name="company"
-                                        value="{{ old('company') }}" placeholder="{{ __('Company') }}">
-                                    @error('company')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
+                        <div class="col-12 col-md-12 mb-2">
+                            <label for="">{{ __('Company') }} </label>
+                            <input type="text" class="form-control" name="company"
+                                value="{{ old('company') }}" placeholder="{{ __('Company') }}">
+                            @error('company')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <label for="">{{ __('First_Name') }} <strong class="text-danger">*</strong></label>
+                            <input type="text" class="form-control" name="first_name"
+                                value="{{ old('first_name') }}" placeholder="{{ __('First_Name') }}">
+                            @error('first_name')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <label for="">{{ __('Last_Name') }} <strong class="text-danger">*</strong></label>
+                            <input type="text" class="form-control" name="last_name"
+                                value="{{ old('last_name') }}" placeholder="{{ __('Last_Name') }}">
+                            @error('last_name')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-                                <div class="col-12 col-md-6 mb-2">
-                                    <label for="">{{ __('First_Name') }} <strong class="text-danger">*</strong></label>
-                                    <input type="text" class="form-control" name="first_name"
-                                        value="{{ old('first_name') }}" placeholder="{{ __('First_Name') }}">
-                                    @error('first_name')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-12 col-md-6 mb-2">
-                                    <label for="">{{ __('Last_Name') }} <strong class="text-danger">*</strong></label>
-                                    <input type="text" class="form-control" name="last_name"
-                                        value="{{ old('last_name') }}" placeholder="{{ __('Last_Name') }}">
-                                    @error('last_name')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <div class="col-12 col-md-6 mb-2">
-                                    <label for="">{{ __('Mobile_number') }} <strong class="text-danger">*</strong></label>
-                                    <input type="text" class="form-control" name="mobile" value="{{ old('mobile') }}"
-                                        placeholder="{{ __('Mobile_number') }}">
-                                    @error('mobile')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-12 col-md-6 mb-2">
-                                    <label for="">{{ __('Email') }}</label>
-                                    <input type="text" class="form-control" name="email" value="{{ old('email') }}"
-                                        placeholder="{{ __('Email') }}">
-                                    @error('email')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-12 col-md-6 mb-2">
-                                    <label for="">{{ __('Password') }} <strong class="text-danger">*</strong></label>
-                                    <div class="d-flex  align-items-center inputBox">
-                                        <div class="input w-100 position-relative">
-                                            <input type="password" id="password" class="form-control" name="password"
-                                                placeholder="******">
-                                            <span class="eye" onclick="myFunction()">
-                                                <i class="fa fa-eye-slash"></i>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    @error('password')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-12 col-md-6 mb-2">
-                                    <label for="">{{ __('Confirm_Password') }}</label>
-                                    <div class="d-flex  align-items-center inputBox">
-                                        <div class="input w-100 position-relative">
-                                            <input type="password" class="form-control" name="password_confirmation"
-                                                placeholder="******" id="confirmPassword">
-                                            <span class="eye" onclick="confirmPassword()">
-                                                <i class="fa fa-eye-slash"></i>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-6 mb-2 py-2">
-                                    <label for="">{{ __('Profile_Photo') }}</label>
-                                    <input type="file" class="form-control-file" name="profile_photo">
-                                </div>
-
-                                @can('customer.store')
-                                <div class="col-12 col-md-6 mb-2 py-2">
-                                    <label for=""></label>
-                                    <button class="btn btn-primary w-100 mt-2  @role('visitor') visitorMessage @endrole">{{ __('Submit') }}</button>
-                                    @endcan
+                        <div class="col-12 col-md-6 mb-2">
+                            <label for="">{{ __('Mobile_number') }} <strong class="text-danger">*</strong></label>
+                            <input type="text" class="form-control" name="mobile" value="{{ old('mobile') }}"
+                                placeholder="{{ __('Mobile_number') }}">
+                            @error('mobile')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <label for="">{{ __('Email') }}</label>
+                            <input type="text" class="form-control" name="email" value="{{ old('email') }}"
+                                placeholder="{{ __('Email') }}">
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <label for="">{{ __('Password') }} <strong class="text-danger">*</strong></label>
+                            <div class="d-flex  align-items-center inputBox">
+                                <div class="input w-100 position-relative">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="******">
+                                    <span class="eye" onclick="myFunction()">
+                                        <i class="fa fa-eye-slash"></i>
+                                    </span>
                                 </div>
                             </div>
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <label for="">{{ __('Confirm_Password') }}</label>
+                            <div class="d-flex  align-items-center inputBox">
+                                <div class="input w-100 position-relative">
+                                    <input type="password" class="form-control" name="password_confirmation"
+                                        placeholder="******" id="confirmPassword">
+                                    <span class="eye" onclick="confirmPassword()">
+                                        <i class="fa fa-eye-slash"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-2 py-2">
+                            <label for="">{{ __('Profile_Photo') }}</label>
+                            <input type="file" class="form-control-file" name="profile_photo">
+                        </div>
 
-                        <button type="submit" class="btn btn-danger text-white w-100">Login</button>
+                        <button class="btn btn-primary w-100 mt-2  ">{{ __('Submit') }}</button>
 
                         <hr>
                         <div class="row">
