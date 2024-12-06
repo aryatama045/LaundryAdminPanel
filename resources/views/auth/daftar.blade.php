26eff26df818f7af17976ea57b5261faad0e2e34
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-06 16:15:12 +0700

    Update daftar.blade.php

--- resources/views/auth/daftar.blade.php
+++ resources/views/auth/daftar.blade.php
@@ -173,7 +173,27 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
             email.value = 'visitor@laundry.com';
             password.value = 'secret@123';
         }
+
+
+        function myFunction() {
+            var x = document.getElementById("password");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+
+        function confirmPassword() {
+            var x = document.getElementById("confirmPassword");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
     </script>
+
 
 </body>
 
