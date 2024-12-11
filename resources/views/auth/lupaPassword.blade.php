96f73c9c7867f7c934b9d1c30645cc8bec43c251
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-11 09:25:56 +0700

    Update lupaPassword.blade.php

--- resources/views/auth/lupaPassword.blade.php
+++ resources/views/auth/lupaPassword.blade.php
@@ -76,6 +76,39 @@ $server  = request()->server('HTTP_SEC_CH_UA_PLATFORM');
         </div>
     </div>
 
+    @if (session('success'))
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: '{{ session('success') }}'
+            })
+        </script>
+    @endif
+
+    @if (session('error'))
+        <script>
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: '{{ session('error') }}',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        </script>
+    @endif
+
 </body>
 
 </html>
