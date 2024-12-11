3ea01c7cd0759f29a4f891ecbd7b35a67f1dd1cb
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-11 14:44:47 +0700

    Update forgotPassword.blade.php

--- resources/views/mail/forgotPassword.blade.php
+++ resources/views/mail/forgotPassword.blade.php
@@ -1,3 +1,7 @@
+
+@php
+$websetting = App\Models\WebSetting::first();
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,56 +26,13 @@
 
     <div class="card" style="max-width: 736px; background: #fff; margin: 12px auto;padding: 12px">
         <div style="text-align: center">
-            <img src="{{ $setting->websit_logo_path ?? asset('web/app_icon.png') }}" width="200px">
+            <img src="{{ $websetting->websiteLogoPath ?? asset('web/logo.png') }}" width="200px">
         </div>
         <h3 style="font-size: 30px; margin-top: 24px;">Thanks for your order!</h3>
-        <p style="font-size: 16px; line-height: 24px; margin: 0; color: #1C1917">Hello {{ $order->customer->name }}</p>
+        <p style="font-size: 16px; line-height: 24px; margin: 0; color: #1C1917">Hello {{ $user->name }}</p>
         <p style="font-size: 16px; line-height: 24px;margin: 0; color:#57534E;">Great news! We have received your order. Check your app or the website to check the status of your order.</p>
         <br>
         <p style="font-size: 16px; line-height: 24px;margin: 0; color:#57534E;">Confirmation of your order details are shown below:</p>
-        <div style="margin-top: 30px">
-            <h4 style="font-size: 18px;color: #1C1917;">Schedule and Location</h4>
-            <div style="background-color: #DCA11215;padding: 16px;font-size: 12px;border-bottom: 1px solid #DCA112;">
-                <div style="clear: both;display: table; width: 100%;">
-                    <div class="float-left" style="float: left; margin-top: 8px;">
-                        <p style="margin: 0;font-size: 14px;">Pick-up</p>
-                        <div style="margin-top: 8px; font-size: 12px;display: flex; align-items: center;">
-                            <img src="https://img.icons8.com/material-outlined/24/000000/calendar--v2.png" style="height: 16px; margin-right: 1px;"/>
-                            {{  $order->pick_date  }}
-                        </div>
-
-                        <div style="margin-top: 8px;font-size: 12px;display: flex; align-items: center;">
-                            <img src="https://img.icons8.com/ios-glyphs/30/000000/time--v1.png" style="height: 16px;margin-right: 1px;"/>
-                            {{ $order->getTime(substr($order->pick_hour, 0, 2)) }}
-                        </div>
-                    </div>
-                    <div class="float-right" style="float: right; margin-top: 8px;">
-                        <p style="margin: 0;font-size: 14px;">Delivery</p>
-                        <div style="margin-top: 8px;font-size: 12px;display: flex; align-items: center;">
-                            <img src="https://img.icons8.com/material-outlined/24/000000/calendar--v2.png" style="height: 16px;margin-right: 1px;"/>
-                            {{ $order->delivery_date }}
-                        </div>
-
-                        <div style="margin-top: 8px;font-size: 12px;display: flex; align-items: center;">
-                            <img src="https://img.icons8.com/ios-glyphs/30/000000/time--v1.png" style="height: 16px;margin-right: 1px;"/>
-                            {{ $order->getTime(substr($order->delivery_hour, 0, 2)) }}
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div style="background-color: #DCA11215; padding: 25px;font-size: 14px;">
-                <p style="margin: 0;font-size: 14px;">Location</p>
-                <div style="margin-top:8px;">
-                    <span style="padding: 6px 4px 2px 4px;background: #ddd; border-radius: 2px;color: #000; margin-right: 6px; box-sizing: border-box">
-                        <img src="https://img.icons8.com/ios/50/000000/marker-o.png" style="height: 13px;"/> <span style="font-size: 12px;">Home</span>
-                    </span>
-                    <span style="color: #44403C;font-size: 12px;">
-                        {{ $order->address->post_code.', '. $order->address->address_line.' '. $order->address->address_line2}}
-                    </span>
-                </div>
-            </div>
-        </div>
 
         <div style="margin-top: 40px">
             <div style="background: #F5F5F4;padding: 16px 8px 16px 16px;">
@@ -80,52 +41,48 @@
             <div style="padding: 0 8px;border-bottom: 1px solid #E7E5E4;font-size: 12px;margin-top: 8px;">
                 <div style=" margin: 8px 0;;display: table; width: 100%;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Order ID</span>
-                    <span style="color: #1C1917;float: right;">#{{ $order->order_code }}</span>
+                    <span style="color: #1C1917;float: right;">#234234</span>
                 </div>
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Date</span>
-                    <span style="color: #1C1917;float: right;">{{ $order->created_at->format('M d, Y') }}</span>
+                    <span style="color: #1C1917;float: right;">123</span>
                 </div>
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Name</span>
-                    <span style="color: #1C1917;float: right;">{{ $order->customer->name }}</span>
+                    <span style="color: #1C1917;float: right;">asd</span>
                 </div>
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Phone Number</span>
-                    <span style="color: #1C1917;float: right;">{{ $order->customer->user->mobile }}</span>
+                    <span style="color: #1C1917;float: right;">asd</span>
                 </div>
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Payment Method</span>
-                    <span style="color: #1C1917;float: right;">{{ $order->payment_status }}</span>
+                    <span style="color: #1C1917;float: right;">asd</span>
                 </div>
             </div>
 
             <div style="padding: 0 8px;border-bottom: 1px solid #57534E;font-size: 12px;">
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
-                    <span style="color: #57534E; float: left;">Quantity ({{ $order->products->count() }} Items)</span>
-                    <span style="color: #1C1917;float: right;">{{ $quantity }}</span>
+                    <span style="color: #57534E; float: left;">Quantity (  Items)</span>
+                    <span style="color: #1C1917;float: right;">3</span>
                 </div>
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Sub Total</span>
-                    <span style="color: #1C1917;float: right;">{{ currencyPosition($order->amount) }}</span>
-                </div>
-                <div style=" margin: 8px 0;;display: table; width: 100%;">
-                    <span style="color: #57534E; float: left;">Pick-up and Delivery Charge</span>
-                    <span style="color: #1C1917;float: right;">{{ currencyPosition($delivery_charge) }}</span>
+                    <span style="color: #1C1917;float: right;">wrf</span>
                 </div>
                 <div style=" margin: 8px 0;;display: table; width: 100%;">
                     <span style="color: #57534E; float: left;">Discount</span>
-                    <span style="color: #EF4444; float: right;">-{{ currencyPosition($order->discount) }}</</span>
+                    <span style="color: #EF4444; float: right;">-345</</span>
                 </div>
             </div>
 
             <div style=" padding: 16px 8px; display: block;">
                 <span style="color: #1C1917;float: left;font-size: 14px;">Paid Amount</span>
-                <span style="color: #1C1917;float: right;font-size: 14px;">{{ currencyPosition($order->total_amount) }}</span>
+                <span style="color: #1C1917;float: right;font-size: 14px;">2324234</span>
             </div>
 
             <div style="text-align: center; margin-top: 30px;">
-                <a href="http://laundry.razinsoft.com/my-order/{{ $order->id }}" target="_blank" style="width: 240px; padding: 16px 0;color: #fff; margin: auto; background: #00B894;text-decoration: none; font-size: 16px;border-radius: 4px; display: block">
+                <a href="http://laundry.razinsoft.com/my-order/1" target="_blank" style="width: 240px; padding: 16px 0;color: #fff; margin: auto; background: #00B894;text-decoration: none; font-size: 16px;border-radius: 4px; display: block">
                 View My Order
                 </a>
             </div>
