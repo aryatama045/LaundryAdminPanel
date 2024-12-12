6b37258ca13e2c1b803321ed6717ab0c5dbc975f
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-12 08:51:14 +0700

    Update OrderController.php

--- app/Http/Controllers/Web/Products/OrderController.php
+++ app/Http/Controllers/Web/Products/OrderController.php
@@ -164,61 +164,26 @@ class OrderController extends Controller
         $data = [];
         foreach($array as $key => $val){
 
-
-
             foreach ($val as $key2 => $val2){
 
-                if(isset($val2['jenis'])){
-                    $slug_jenis = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['jenis'])));
-                    $jenis_data = JenisBarangModel::firstOrCreate(['jenisbarang_nama' => $val2['jenis'], 'jenisbarang_slug' => $slug_jenis, 'jenisbarang_ket' => '']);
-                    $jenis_id   = $jenis_data->jenisbarang_id;
-                }else{
-                    $jenis_id = null;
-                }
+                $tanggal_nota = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($val2['tanggal_nota']);
 
-                if(isset($val2['kategori'])){
-                    $slug_kategori  = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['kategori'])));
+                $order->nomor_nota        = $val2['nomor_nota'];
+                $order->tanggal_nota      = $tanggal_nota;
+                $order->nama_customer     = $val2['nama_customer'];
+                $order->nama_barang       = $val2['nama_barang'];
+                $order->qty               = $val2['qty'];
+                $order->satuan            = $val2['satuan'];
+                $order->part_number       = $val2['part_number'];
 
-                    $kategori       = KategoriModel::firstOrCreate(['kategori_nama' => $val2['kategori'], 'kategori_slug' => $slug_kategori, 'kategori_ket' => '']);
-                    $kategori_id    = $kategori->kategori_id;
-                }else{
-                    $kategori_id    = null;
-                }
+                $order->save();
 
-                if(isset($val2['merk'])){
-                    $slug_merk = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['merk'])));
-                    $merk       = MerkModel::firstOrCreate(['merk_nama' => $val2['merk'], 'merk_slug' => $slug_merk, 'merk_keterangan' => '']);
-                    $merk_id    = $merk->merk_id;
-                }else{
-                    $merk_id    = null;
-                }
-
-                if(isset($val2['satuan'])){
-                    $slug_satuan = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['satuan'])));
-                    $satuan_data    = SatuanModel::firstOrCreate(['satuan_nama' => $val2['satuan'], 'satuan_slug' => $slug_satuan, 'satuan_keterangan' => '']);
-                    $satuan_id      = $satuan_data->satuan_id;
-                }else{
-                    $satuan_id      = null;
-                }
-
-                $product        = BarangModel::create();
-
-
-                $product->barang_nama       = $val2['name'];
-                $product->barang_slug       = $slug_barang;
-                $product->jenisbarang_id    = $jenis_id;
-                $product->kategori_id       = $kategori_id;
-                $product->merk_id           = $merk_id;
-                $product->satuan_id         = $satuan_id;
-                $product->barang_spek       = $val2['spek'];
-                $product->barang_stok       = 0;
-                $product->barang_harga      = $val2['harga'];
-
-                $product->save();
+                array_push($data, $order);
 
             }
 
         }
+        dd($data);
 
 
         return redirect('admin/barang')->with('create_message', 'Product imported successfully');
