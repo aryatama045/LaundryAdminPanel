0d82775f0ca016e8300f9bc56aea30940ac837fc
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-12 14:02:24 +0700

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -44,13 +44,13 @@
                             <table class="table table-bordered {{ session()->get('local') }}" id="myTable">
                                 <thead>
                                     <tr>
-                                        <th scope="col">{{ __('Order') . ' ' . __('Id') }} </th>
-                                        <th scope="col">{{ __('Order') . ' ' . __('By') }}</th>
-                                        <th scope="col">{{ __('Order') . ' ' . __('At') }}</th>
-                                        <th scope="col">{{ __('Pickup') . ' ' . __('Date') }}</th>
-                                        <th scope="col">{{ __('Delivery') . ' ' . __('Date') }}</th>
-                                        <th scope="col">{{ __('Amount') }}</th>
-                                        <th scope="col">{{ __('Order') . ' ' . __('Status') }}</th>
+                                        <th scope="col">Tanggal Nota </th>
+                                        <th scope="col">Nomor Nota</th>
+                                        <th scope="col">Nama Customer</th>
+                                        <th scope="col">Nama Barang</th>
+                                        <th scope="col">Qty</th>
+                                        <th scope="col">Satuan</th>
+                                        <th scope="col">Part Number</th>
                                         @canany(['order.show', 'order.print.invioce'])
                                             <th scope="col" class="px-2">{{ __('Actions') }}</th>
                                         @endcanany
@@ -58,75 +58,21 @@
                                 </thead>
                                 <tbody>
                                     @foreach ($orders as $order)
-                                        <tr class="{{ $order->is_show ? '' : 'bg-color' }}">
-                                            <td class="py-1">{{ $order->prefix . $order->order_code }}</td>
+                                        <tr class="{{ $order->customer_id ? '' : 'bg-color' }}">
                                             <td class="py-1">
-                                                {{ $order->customer->user ? $order->customer->user->name : 'N/A' }}
+                                                {{ $order->tanggal_nota->format(d/m/Y') }}
                                             </td>
+                                            <td class="py-1">{{ $order->nomor_nota }}</td>
                                             <td class="py-1">
-                                                {{ $order->created_at->format('M d, Y') }}
+                                                {{ $order->nama_customer ? $order->nama_customer : 'N/A' }}
                                             </td>
+                                            <td class="py-1">{{ $order->nama_barang }}</td>
+                                            <td class="py-1">{{ $order->qty }}</td>
+                                            <td class="py-1">{{ $order->satuan }}</td>
+                                            <td class="py-1">{{ $order->part_number }}</td>
 
-                                            <td class="py-1">
-                                                {{ \Carbon\Carbon::parse($order->pick_date)->format('M d, Y') }}
-                                            </td>
-                                            <td class="py-1">
-                                                {{ \Carbon\Carbon::parse($order->delivery_date)->format('M d, Y') }}
-                                            </td>
-                                            <td class="py-1">{{ currencyPosition($order->total_amount) }}</td>
-                                            <td class="py-1">{{ __($order->order_status) }}</td>
                                             @canany(['order.show', 'order.print.invioce'])
                                                 <td class="p-1 ">
-                                                    <a href="#address{{ $order->id }}" data-toggle="modal"
-                                                        class="btn btn-primary btn-sm mb-1">
-                                                        <i class="fa fa-address-card"></i>
-                                                    </a>
-                                                    <!-- Modal -->
-                                                    <div class="modal fade" id="address{{ $order->id }}" tabindex="-1"
-                                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                        <div class="modal-dialog">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel">
-                                                                        {{ __('Order') . ' ' . __('Address') }}</h5>
-                                                                    <button type="button" class="close" data-dismiss="modal"
-                                                                        aria-label="Close">
-                                                                        <span aria-hidden="true">&times;</span>
-                                                                    </button>
-                                                                </div>
-                                                                <div class="modal-body">
-                                                                        <p>{{ __('Order') . ' ' . __('Id') }}:
-                                                                            <strong>{{ $order->prefix . $order->order_code }}</strong>
-                                                                        </p>
-                                                                        <p>
-                                                                            {{ __('Order') . ' ' . __('From') }}:
-                                                                            <strong>{{ $order->address->address_name }}</strong>
-                                                                        </p>
-                                                                        <p>
-                                                                            {{ __('Post_Code') }}:
-                                                                            <strong>{{ $order->address->post_code }}</strong>
-                                                                        </p>
-                                                                        <p>
-                                                                            {{ __('Address_line') }} 1:
-                                                                            <strong>{{ $order->address->address_line }}</strong>
-                                                                        </p>
-
-                                                                        <p>
-                                                                            {{ __('Address_line') }} 2:
-                                                                            <strong>{{ $order->address->address_line2 }}</strong>
-                                                                        </p>
-                                                                        <p>
-                                                                            {{ __('Delivery') . ' ' . __('Note') }}:
-                                                                            <strong>{{ $order->address->delivery_note }}</strong>
-                                                                        </p>
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="button" class="btn btn-dark btn-sm"
-                                                                        data-dismiss="modal">{{ __('Close') }}</button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
 
                                                     @can('order.show')
                                                         <a href="{{ route('order.show', $order->id) }}"
@@ -165,16 +111,10 @@
             <form class="modal-dialog-scrollable" enctype='multipart/form-data' action="{{ url('orders/imports') }}" method="POST">
             @csrf
             <div class="card-body">
-                <!--<a class="btn btn-danger" onclick="pdf()"><i class="fa fa-file-pdf-o"></i> PDF</a>-->
-                <!--<br><br>-->
-                <!--<hr>-->
-                <!--<br><br>-->
-
                 <div class="form-group">
                     <label class="mb-1">{{ __('Import') . ' ' . __('Excel') }}</label>
                     <input name="import_data" type="file" />
                 </div>
-
             </div>
 
             <div class="card-footer">
