21cdda402b9a77148f6239fc766b15d0fa79c74d
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-14 17:01:47 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -115,7 +115,6 @@ Route::middleware(['auth', 'role:admin|visitor|customer|root', 'permission_check
 
     // Garansi routes
     Route::get('/garansi', [CustomerGaransiController::class, 'index'])->name('garansi.index');
-    Route::get('/garansi/getdata', [CustomerGaransiController::class, 'getdata'])->name('garansi.getdata');
 
     Route::get('/garansi/{garansi}/show', [CustomerGaransiController::class, 'show'])->name('garansi.show');
     Route::get('/garansi/create', [CustomerGaransiController::class, 'create'])->name('garansi.create');
@@ -210,6 +209,8 @@ Route::middleware(['auth', 'role:admin|visitor|customer|root', 'permission_check
     });
 });
 
+
+Route::get('/garansi/getdata', [CustomerGaransiController::class, 'getdata'])->name('garansi.getdata');
 
 
 // Order Routes
