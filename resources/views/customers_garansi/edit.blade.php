a77402bf09ac9a288a73308a61818854b5c9b7c9
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-16 14:14:39 +0700

    Update edit.blade.php

--- resources/views/customers_garansi/edit.blade.php
+++ resources/views/customers_garansi/edit.blade.php
@@ -44,7 +44,7 @@
                     <div class="w-100">
                         <h2 class="float-left">{{ __('Tambah'). ' '. __('Proteksi') }} - {{ $order->nama_barang }}</h2>
                         <div class="text-right">
-                            <a class="btn btn-light" href="{{ route('customer.index') }}"> {{ __('Back') }} </a>
+                            <a class="btn btn-light" href="{{ route('garansi.index') }}"> {{ __('Back') }} </a>
                         </div>
                     </div>
                 </div>
