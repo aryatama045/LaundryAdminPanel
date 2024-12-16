e0d8595688f06d15da9fac7dab4dcee65488a8f5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-16 14:03:12 +0700

    Update edit.blade.php

--- resources/views/customers_garansi/edit.blade.php
+++ resources/views/customers_garansi/edit.blade.php
@@ -50,8 +50,7 @@
                 </div>
                 <div class="card-body">
                     <form action="{{ route('garansi.update', $order->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('put')
+                        @csrf @method('put')
                         <div class="row">
                             <!-- nomor_nota -->
                             <div class="col-12 col-md-6 mb-4">
