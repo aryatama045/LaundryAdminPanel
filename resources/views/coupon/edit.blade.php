d817c0e5e0b480b9593e69d4e357ddf0fd8d1fbc
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-12-20 16:06:18 +0700

    Update edit.blade.php

--- resources/views/coupon/edit.blade.php
+++ resources/views/coupon/edit.blade.php
@@ -23,11 +23,11 @@
                         <form @role('root|admin') @can('coupon.update') action="{{ route('coupon.update', $coupon->id) }}" @endcan @endrole method="POST"> @csrf @method('put')
                             <div class="row">
                                 <div class="col-12 col-md-6">
-                                    <label class="mb-1">{{ __('Coupon'). ' '.__('Code') }}</label>
-                                    <x-input name="code" type="text" value="{{ $coupon->code }}" placeholder="Coupon code"/>
+                                    <label class="mb-1">{{  '.__('Code') }}</label>
+                                    <x-input name="code" type="text" value="{{ $coupon->code }}" placeholder="code"/>
                                 </div>
 
-                                <div class="col-12 col-md-6">
+                                <!-- <div class="col-12 col-md-6">
                                     <label class="mb-1">{{ __('Select').' '.__('Discount_Type') }}</label>
                                     <select name="discount_type" class="form-control @error('discount_type') is-invalid @enderror">
                                         <option value="">{{ __('Select').' '.__('Discount_Type') }}</option>
@@ -72,18 +72,18 @@
                                             <x-input type="time" value="{{ Carbon\Carbon::parse($coupon->expired_at)->format('H:i') }}" name="expired_time"/>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
 
                                 <div class="col-12">
                                     <div class="row">
-                                        <div class="col-12 col-md-6">
+                                        <!-- <div class="col-12 col-md-6">
                                             <label for="yes">
                                                 <input type="radio" id="yes" name="notify" value="1"> {{ __('Notify_All_Customer') }}.
                                             </label>
                                             <label for="no" class="ml-3">
                                                 <input type="radio" id="no" name="notify" value="0"> {{ __('not_need') }}
                                             </label>
-                                        </div>
+                                        </div> -->
                                         @can('coupon.update')
                                         <div class="col-12 col-md-6 text-right">
                                             <button type="submit" class="btn btn-primary px-5 @role('visitor') visitorMessage @endrole">{{ __('Update') }}</button>
