afb323747db67eaa91189c81f20499a101f73041
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-01-12 10:35:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -121,8 +121,8 @@ Route::middleware(['auth', 'role:admin|visitor|customer|root', 'permission_check
     Route::post('/garansi', [CustomerGaransiController::class, 'store'])->name('garansi.store');
     Route::post('/garansi/{garansi}/proses', [CustomerGaransiController::class, 'proses_action'])->name('garansi.proses_action');
 
-    Route::post('/garansi/{garansi}/disetujui', [CustomerGaransiController::class, 'disetujui'])->name('garansi.disetujui');
-    Route::post('/garansi/{garansi}/ditolak', [CustomerGaransiController::class, 'ditolak'])->name('garansi.ditolak');
+    Route::put('/garansi/{garansi}/disetujui', [CustomerGaransiController::class, 'disetujui'])->name('garansi.disetujui');
+    Route::put('/garansi/{garansi}/ditolak', [CustomerGaransiController::class, 'ditolak'])->name('garansi.ditolak');
 
 
     Route::get('/garansi/{garansi}/edit', [CustomerGaransiController::class, 'edit'])->name('garansi.edit');
