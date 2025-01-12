dfd06b33edc069e553a817f8c75ca36269d35a02
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-01-12 14:18:55 +0700

    Update CustomerKlaimRepository.php

--- app/Repositories/CustomerKlaimRepository.php
+++ app/Repositories/CustomerKlaimRepository.php
@@ -31,9 +31,15 @@ class CustomerKlaimRepository extends Repository
     {
         $searchKey = \request('search');
         
-        $user_id = auth()->user()->getRelations('roles');
-        dd($user_id);
-        $user_id = $user_id['roles'][0]->name;
+        // $user_id = auth()->user()->getRelations('roles');
+        
+        // $user_id = $user_id['roles'][0]->name;
+
+        $userid = auth()->user()->id;
+
+        dd($userid);
+
+        $cst = Customer::where('user_id', $userid)->first();
 
 
         $klaims = $this->model()::query();
