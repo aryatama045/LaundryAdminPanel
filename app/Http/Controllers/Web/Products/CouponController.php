c14c1851ce794c971911e6a9a2a9203ce80aeba9
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-01-12 09:27:06 +0700

    Update CouponController.php

--- app/Http/Controllers/Web/Products/CouponController.php
+++ app/Http/Controllers/Web/Products/CouponController.php
@@ -30,16 +30,6 @@ class CouponController extends Controller
     {
         (new CouponRepository())->storeByRequest($request);
 
-        // if ($request->notify) {
-
-        //     $notificationManage = NotificationManage::where('name', 'coupon_notify')->first();
-        //     $keys = (new DeviceKeyRepository())->getAll()->pluck('key')->toArray();
-
-        //     if ($notificationManage?->is_active) {
-        //         $message = $notificationManage->message;
-        //         (new NotificationServices($message, $keys, 'Coupon Discount'));
-        //     }
-        // }
 
         return redirect()->route('coupon.index')->with('success', 'Code is added successfully.');
     }
@@ -53,16 +43,6 @@ class CouponController extends Controller
     {
         (new CouponRepository())->updateByRequest($request, $coupon);
 
-        // if ($request->notify) {
-
-        //     $notificationManage = NotificationManage::where('name', 'coupon_notify')->first();
-        //     $keys = (new DeviceKeyRepository())->getAll()->pluck('key')->toArray();
-
-        //     if ($notificationManage?->is_active) {
-        //         $message = $notificationManage->message;
-        //         (new NotificationServices($message, $keys, 'Coupon Discount'));
-        //     }
-        // }
 
         return redirect()->route('coupon.index')->with('success', 'Coupon is updated successfully.');
     }
@@ -74,78 +54,22 @@ class CouponController extends Controller
 
         $array= Excel::toArray(new BarangImport, $file);
 
-        dd($array);
-
         $data = [];
         foreach($array as $key => $val){
 
             foreach ($val as $key2 => $val2){
 
-                if(isset($val2['jenis'])){
-                    $slug_jenis = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['jenis'])));
-                    $jenis_data = JenisBarangModel::firstOrCreate(['jenisbarang_nama' => $val2['jenis'], 'jenisbarang_slug' => $slug_jenis, 'jenisbarang_ket' => '']);
-                    $jenis_id   = $jenis_data->jenisbarang_id;
-                }else{
-                    $jenis_id = null;
-                }
+                $code_data        = Coupon::firstOrNew(['code'=>$val2['code'] ]);
 
-                if(isset($val2['kategori'])){
-                    $slug_kategori  = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['kategori'])));
+                $code_data->code       = $val2['code'];
 
-                    $kategori       = KategoriModel::firstOrCreate(['kategori_nama' => $val2['kategori'], 'kategori_slug' => $slug_kategori, 'kategori_ket' => '']);
-                    $kategori_id    = $kategori->kategori_id;
-                }else{
-                    $kategori_id    = null;
-                }
-
-                if(isset($val2['merk'])){
-                    $slug_merk = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['merk'])));
-                    $merk       = MerkModel::firstOrCreate(['merk_nama' => $val2['merk'], 'merk_slug' => $slug_merk, 'merk_keterangan' => '']);
-                    $merk_id    = $merk->merk_id;
-                }else{
-                    $merk_id    = null;
-                }
-
-                if(isset($val2['satuan'])){
-                    $slug_satuan = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['satuan'])));
-                    $satuan_data    = SatuanModel::firstOrCreate(['satuan_nama' => $val2['satuan'], 'satuan_slug' => $slug_satuan, 'satuan_keterangan' => '']);
-                    $satuan_id      = $satuan_data->satuan_id;
-                }else{
-                    $satuan_id      = null;
-                }
-
-                $product        = BarangModel::firstOrNew([ 'barang_nama'=>$val2['name'] ]);
-
-                if($val2['image'])
-                    $product->barang_gambar = $val2['image'];
-                else
-                    $product->barang_gambar = 'image.png';
-
-
-                $random = Str::random(13);
-
-                $codeProduct = 'BRG-'.$random;
-                $slug_barang = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $val2['name'])));
-
-                $product->barang_kode       = $codeProduct;
-                $product->barang_nama       = $val2['name'];
-                $product->barang_slug       = $slug_barang;
-                $product->jenisbarang_id    = $jenis_id;
-                $product->kategori_id       = $kategori_id;
-                $product->merk_id           = $merk_id;
-                $product->satuan_id         = $satuan_id;
-                $product->barang_spek       = $val2['spek'];
-                $product->barang_stok       = 0;
-                $product->barang_harga      = $val2['harga'];
-
-                $product->save();
+                $code_data->save();
 
             }
 
         }
 
-        // Excel::import(new AdjustmentStokExcelImport, $request->file('file'));
-
-        return redirect('admin/barang')->with('create_message', 'Product imported successfully');
+        return redirect()->route('coupon.index')->with('success', 'Coupon is imported successfully.');
+        // return redirect('admin/barang')->with('create_message', 'Product imported successfully');
     }
 }
