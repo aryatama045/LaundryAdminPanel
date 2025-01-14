47c42dfa2c8283eccedeebd497ea84716b30459e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-01-14 11:58:49 +0700

    Update CustomerKlaimController.php

--- app/Http/Controllers/Web/Customers/CustomerKlaimController.php
+++ app/Http/Controllers/Web/Customers/CustomerKlaimController.php
@@ -144,7 +144,7 @@ class CustomerKlaimController extends Controller
 
                 ->addColumn('waktu_rusak', function ($row) {
                     $klaim    = CustomerKlaims::where('id', $row->klaim_id)->first();
-                    if($klaim->waktu_pemasangan){
+                    if(!$klaim->waktu_pemasangan){
                         $result =  date('H:i:s',strtotime($klaim->waktu_pemasangan));
                     }else{
                         $result = '';
@@ -154,7 +154,7 @@ class CustomerKlaimController extends Controller
                 })
                 ->addColumn('tanggal_rusak', function ($row) {
                     $klaim    = CustomerKlaims::where('id', $row->klaim_id)->first();
-                    if($klaim->tanggal_pemasangan){
+                    if(!$klaim->tanggal_pemasangan){
                         $result =  date('d-m-Y', strtotime($klaim->tanggal_pemasangan));
                     }else{
                         $result = '';
