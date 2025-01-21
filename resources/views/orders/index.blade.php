5613a35e84312c3c569c29ce25e69c6e21d5e3a0
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-01-21 11:26:44 +0700

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -157,7 +157,7 @@ $(document).ready(function() {
 
     $('body').on('click', '.editProduct', function () {
         var order_id = $(this).data('id');
-        $.get("{{ route('order.getDataRetur'."+ order_id+") }}" , function (data) {
+        $.get("{{ route('orders.index') }}" +'/' + order_id +'/data_retur', function (data) {
 
             $('#modelHeading').html("Retur");
 
